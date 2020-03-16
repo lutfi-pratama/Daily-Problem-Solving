4621a6ef0c89e3227d502ec09fd19654b3c3bf64
lutfi-pratama <60962944+lutfi-pratama@users.noreply.github.com>
2020-03-16 11:33:51 +0700

    Create ReverseString.js

---
+++ ReverseString.js
@@ -0,0 +1,12 @@
+// HOW TO REVERSE STRING ON THE JAVASCRIPT
+
+function reverseString (str){
+
+  // var changeArr = str.split("");
+  // var reverseArr = changeArr.reverse();
+  // var changeStr = reverseArr.join("");
+
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Lutfi Pratama"));
